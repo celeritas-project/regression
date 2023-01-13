be8c40d04c826e4ed21ff8a3392e7802b6115be1
Seth R Johnson <johnsonsr@ornl.gov>
2023-01-13 14:17:01 -0500

    Run summit and crusher with new toolchains

--- run-crusher.sh
+++ run-crusher.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 #SBATCH -A csc333_crusher
-#SBATCH -t 0:59:59
+#SBATCH -t 1:59:59
 #SBATCH -N 1
 #SBATCH -J celer-regression
 #SBATCH -o crusher-%J.out
@@ -12,7 +12,7 @@ if [ -z "$SLURM_JOB_ID" ]; then
   exit $?
 fi
 
-source /ccs/home/s3j/.local/src/celeritas/scripts/env/crusher.sh
+source /ccs/home/s3j/.local/src/celeritas-crusher/scripts/env/crusher.sh
 
 echo "Running on $HOSTNAME at $(date)"
 python3 run-problems.py crusher
