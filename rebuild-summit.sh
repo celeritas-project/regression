cb4b65dc7c885adb03ecc77658f6b22acdfd4d5a
Seth R Johnson <johnsonsr@ornl.gov>
2023-10-23 12:34:24 -0400

    Add summit helper scripts

---
+++ rebuild-summit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -ex
+
+source $HOME/.local/src/celeritas-summit/scripts/env/summit.sh
+cd /gpfs/alpine/proj-shared/csc404/celeritas/build-ndebug \
+  && find . -name '*.cu.o' -exec rm {} + \
+  && cmake -UCeleritas_GIT_DESCRIBE . \
+  && ninja celer-sim
+cd /gpfs/alpine/proj-shared/csc404/celeritas/build-ndebug-novg \
+  && cmake -UCeleritas_GIT_DESCRIBE . \
+  && ninja celer-sim
