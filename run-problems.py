f8ef1cc33c1c01298505bf02e92b08e5dda7e8dd
Seth R Johnson <johnsonsr@ornl.gov>
2024-04-28 19:30:06 -0400

    Add CPU-only local tests of cms

--- run-problems.py
+++ run-problems.py
@@ -97,8 +97,9 @@ class Wildstyle(System):
 
 
 class Local(System):
+    _CELER_ROOT = Path(environ['HOME']) / 'Code' / 'celeritas-frontier'
     build_dirs = {
-        "orange": Path("/Users/seth/Code/celeritas-temp/build"),
+        "orange": _CELER_ROOT / 'build-ndebug-560'
     }
     name = "testing"
     num_jobs = 1
@@ -312,6 +313,7 @@ base_input = {
         "direction": {"distribution": "isotropic"},
         "primaries_per_event": 1300,  # 13 TeV
     },
+    "write_track_counts": True,
 }
 
 use_geant = {
@@ -379,31 +381,31 @@ use_vecgeom = {"_geometry": "vecgeom"}
 
 # List of list of setting dictionaries
 problems = [
-    [testem15],
-    [testem15, use_field],
-    [testem15, use_msc, use_field],
-    [testem15, use_msc, use_field, use_vecgeom],
-    [simple_cms, use_msc],
-    [simple_cms, use_field],
+#    [testem15],
+#    [testem15, use_field],
+#    [testem15, use_msc, use_field],
+#    [testem15, use_msc, use_field, use_vecgeom],
+#    [simple_cms, use_msc],
+#    [simple_cms, use_field],
     [simple_cms, use_field, use_msc],
-    [simple_cms, use_field, use_msc, use_vecgeom],
-    [testem3],
-    [testem3, use_vecgeom],
-    [testem3, use_field],
-    [testem3, use_msc],
-    [testem3, use_field, use_msc],
-    [testem3, use_field, use_msc, use_vecgeom],
-    [full_cms],
-    [full_cms, use_field, use_msc],
+#    [simple_cms, use_field, use_msc, use_vecgeom],
+#    [testem3],
+#    [testem3, use_vecgeom],
+#    [testem3, use_field],
+#    [testem3, use_msc],
+#    [testem3, use_field, use_msc],
+#    [testem3, use_field, use_msc, use_vecgeom],
+#    [full_cms],
+#    [full_cms, use_field, use_msc],
 ]
 
 # Run again with sync on for detailed GPU timing
 sync_problems = [
-    [testem15, use_field],
-    [testem15, use_field, use_vecgeom],
-    [testem3, use_field, use_msc],
-    [testem3, use_field, use_msc, use_vecgeom],
-    [full_cms, use_field, use_msc],
+#    [testem15, use_field],
+#    [testem15, use_field, use_vecgeom],
+#    [testem3, use_field, use_msc],
+#    [testem3, use_field, use_msc, use_vecgeom],
+#    [full_cms, use_field, use_msc],
 ]
 
 def recurse_updated(d, other):
